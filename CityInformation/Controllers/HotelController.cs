769f89e009d3d444c371e4fcb173e1270cb01110
USER <USER@LAPTOP-6L82N2OA>
2022-12-11 20:47:34 +0600

    Add project files.

---
+++ CityInformation/Controllers/HotelController.cs
@@ -0,0 +1,87 @@
+﻿using BLL.DTO;
+using BLL.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace CityInformation.Controllers
+{
+    public class HotelController : ApiController
+    {     
+        [Route("api/hotels")]
+        public HttpResponseMessage Get()
+        {
+            try
+            {
+                var data = HotelService.Get();
+                return Request.CreateResponse(HttpStatusCode.OK, data);
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex.Message);
+            }
+        }
+
+        [Route("api/hotels/add")]
+        [HttpPost]
+        public HttpResponseMessage Add(HotelDTO hotel)
+        {
+            try
+            {
+                var data = HotelService.Add(hotel);
+                if (data != null)
+                {
+                    return Request.CreateResponse(HttpStatusCode.OK, data);
+                }
+                return Request.CreateResponse(HttpStatusCode.BadRequest, new { });
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex.Message);
+            }
+        }
+        [Route("api/hotels/{id}")]
+        [HttpGet]
+        public HttpResponseMessage Get(int id)
+        {
+            try
+            {
+                return Request.CreateResponse(HttpStatusCode.OK, HotelService.Get(id));
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex.Message);
+            }
+        }
+        [Route("api/hotels/update/{id}")]
+        [HttpGet]
+        public HttpResponseMessage Update(HotelDTO hotel)
+        {
+            try
+            {
+                return Request.CreateResponse(HttpStatusCode.OK, HotelService.Update(hotel));
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex.Message);
+            }
+        }
+        [Route("api/hotels/delete/{id}")]
+        [HttpGet]
+        public HttpResponseMessage Delete(int id)
+        {
+            try
+            {
+                return Request.CreateResponse(HttpStatusCode.OK, HotelService.Delete(id));
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex.Message);
+            }
+        }
+
+    }
+}
